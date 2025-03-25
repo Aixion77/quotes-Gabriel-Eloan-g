c3d757792cd813b22cc26f17f7360af1d3bf3d8d
olodiont <118073404+olodiont@users.noreply.github.com>
2025-03-25 11:46:23 +0100

    Initial quotes program

--- quotes.c
+++ quotes.c
@@ -3,3 +3,12 @@
 //
 
 #include "quotes.h"
+
+void print_quote(char ** quotes, int index) {
+    printf("%s\n", quotes[index]);
+}
+
+void print_random_quote(char ** quotes) {
+    int index = rand() % MAX_QUOTES;
+    print_quote(quotes, index);
+}
