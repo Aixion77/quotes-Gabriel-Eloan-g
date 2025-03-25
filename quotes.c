8b66a2535336ff52b83bea325f3c496e3e19ec8e
olodiont <118073404+olodiont@users.noreply.github.com>
2025-03-25 11:45:49 +0100

    Initial quotes program

---
+++ quotes.c
@@ -0,0 +1,5 @@
+//
+// Created by 33695 on 25/03/2025.
+//
+
+#include "quotes.h"
---
+++ quotes.h
@@ -0,0 +1,8 @@
+//
+// Created by 33695 on 25/03/2025.
+//
+
+#ifndef QUOTES_H
+#define QUOTES_H
+
+#endif //QUOTES_H
